688ef1f5925a50f91bf87fcead3b450f6421a5d7
fugu <77485093+qwel64@users.noreply.github.com>
2024-07-02 19:38:03 +0900

    Add files via upload

---
+++ remove_restriction.cpp
@@ -0,0 +1,64 @@
+#include <windows.h>
+#include <aclapi.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+    PACL pOldDACL = NULL, pNewDACL = NULL;
+    PSECURITY_DESCRIPTOR pSD = NULL;
+    EXPLICIT_ACCESS_W ea;
+
+    LPCWSTR path = L"C:\\Path";
+
+    DWORD dwRes = GetNamedSecurityInfoW(
+        path,
+        SE_FILE_OBJECT,
+        DACL_SECURITY_INFORMATION,
+        NULL,
+        NULL,
+        &pOldDACL,
+        NULL,
+        &pSD
+    );
+
+    if (dwRes != ERROR_SUCCESS) {
+        MessageBoxW(NULL, L"GetNamedSecurityInfo failed", L"Error", MB_OK | MB_ICONERROR);
+        return 1;
+    }
+
+    ZeroMemory(&ea, sizeof(EXPLICIT_ACCESS_W));
+    ea.grfAccessPermissions = GENERIC_ALL;
+    ea.grfAccessMode = SET_ACCESS;
+    ea.grfInheritance = SUB_CONTAINERS_AND_OBJECTS_INHERIT;
+    ea.Trustee.TrusteeForm = TRUSTEE_IS_NAME;
+    ea.Trustee.ptstrName = const_cast<LPWSTR>(L"Everyone");
+
+    dwRes = SetEntriesInAclW(1, &ea, pOldDACL, &pNewDACL);
+    if (dwRes != ERROR_SUCCESS) {
+        MessageBoxW(NULL, L"SetEntriesInAcl failed", L"Error", MB_OK | MB_ICONERROR);
+        LocalFree(pSD);
+        return 1;
+    }
+
+    dwRes = SetNamedSecurityInfoW(
+        const_cast<LPWSTR>(path),
+        SE_FILE_OBJECT,
+        DACL_SECURITY_INFORMATION,
+        NULL,
+        NULL,
+        pNewDACL,
+        NULL
+    );
+
+    if (dwRes != ERROR_SUCCESS) {
+        MessageBoxW(NULL, L"SetNamedSecurityInfo failed", L"Error", MB_OK | MB_ICONERROR);
+        LocalFree(pNewDACL);
+        LocalFree(pSD);
+        return 1;
+    }
+
+    MessageBoxW(NULL, L"Access restriction removed successfully", L"Success", MB_OK | MB_ICONINFORMATION);
+
+    LocalFree(pNewDACL);
+    LocalFree(pSD);
+
+    return 0;
+}
