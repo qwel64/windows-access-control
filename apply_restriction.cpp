a484eafa704dd74b4dd1cf17eac048177dc198d3
fugu <77485093+qwel64@users.noreply.github.com>
2024-07-02 19:39:30 +0900

    Update apply_restriction.cpp

--- apply_restriction.cpp
+++ apply_restriction.cpp
@@ -1,17 +1,13 @@
 #include <windows.h>
 #include <aclapi.h>
 
-// Windowsアプリケーションのエントリーポイント
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
     PACL pOldDACL = NULL, pNewDACL = NULL;
     PSECURITY_DESCRIPTOR pSD = NULL;
     EXPLICIT_ACCESS_W ea;
 
-    // 制限したいパス
-
     LPCWSTR path = L"C:\\Path";
     
-    // 現在のセキュリティ記述子を取得
     DWORD dwRes = GetNamedSecurityInfoW(
         path,
         SE_FILE_OBJECT,
@@ -28,7 +24,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         return 1;
     }
 
-    // 拒否アクセス制御エントリ (ACE) を作成
     ZeroMemory(&ea, sizeof(EXPLICIT_ACCESS_W));
     ea.grfAccessPermissions = GENERIC_ALL;
     ea.grfAccessMode = DENY_ACCESS;
@@ -36,7 +31,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     ea.Trustee.TrusteeForm = TRUSTEE_IS_NAME;
     ea.Trustee.ptstrName = const_cast<LPWSTR>(L"Everyone");
 
-    // 新しい ACL を作成
     dwRes = SetEntriesInAclW(1, &ea, pOldDACL, &pNewDACL);
     if (dwRes != ERROR_SUCCESS) {
         MessageBoxW(NULL, L"SetEntriesInAcl failed", L"Error", MB_OK | MB_ICONERROR);
@@ -44,7 +38,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         return 1;
     }
 
-    // セキュリティ記述子を更新
     dwRes = SetNamedSecurityInfoW(
         const_cast<LPWSTR>(path),
         SE_FILE_OBJECT,
@@ -64,7 +57,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
     MessageBoxW(NULL, L"Access restriction applied successfully", L"Success", MB_OK | MB_ICONINFORMATION);
 
-    // リソースの解放
     LocalFree(pNewDACL);
     LocalFree(pSD);
 
